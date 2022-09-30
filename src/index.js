0a7dd4018161f35b9ffc1da6ecbdc095f78f4ece
JuanFlorez1326 <juanks339@gmail.com>
2022-09-30 11:48:18 -0500

    Wolves React Page

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Home } from './Components/Pages/Home/Home'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ src/lobos.js
@@ -0,0 +1,27 @@
+import Lobo1 from './Images/Lobos/lobo1.jpeg';
+import Lobo2 from './Images/Lobos/lobo2.jpeg';
+import Lobo3 from './Images/Lobos/lobo3.jpeg';
+import Lobo4 from './Images/Lobos/lobo4.jpeg';
+import Lobo5 from './Images/Lobos/lobo5.jpeg';
+import Lobo6 from './Images/Lobos/lobo6.jpeg';
+import Lobo7 from './Images/Lobos/lobo7.jpeg';
+import Lobo8 from './Images/Lobos/lobo8.jpeg';
+import Lobo9 from './Images/Lobos/lobo9.jpeg';
+import Lobo10 from './Images/Lobos/lobo10.jpeg';
+import Lobo11 from './Images/Lobos/lobo11.jpeg';
+import Lobo12 from './Images/Lobos/lobo12.jpeg';
+
+export const lobos = [
+  {id:1,name:"Wolf  1",URL: Lobo1},
+  {id:2,name:"Wolf 2",URL: Lobo2},
+  {id:3,name:"Wolf 3",URL: Lobo3},
+  {id:4,name:"Wolf 4",URL: Lobo4},
+  {id:5,name:"Wolf 5",URL: Lobo5},
+  {id:6,name:"Wolf 6",URL: Lobo6},
+  {id:7,name:"Wolf 7",URL: Lobo7},
+  {id:8,name:"Wolf 8",URL: Lobo8},
+  {id:9,name:"Wolf 9",URL: Lobo9},
+  {id:10,name:"Wolf 10",URL: Lobo10},
+  {id:11,name:"Wolf 11",URL: Lobo11},
+  {id:12,name:"Wolf 12",URL: Lobo12},
+];
